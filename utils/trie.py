80f53704c7c7afe94d7d5cfaa231a47933be42fb
LiXianyao <buptlixianyao@163.com>
2019-04-26 11:06:11 +0800

    阅读trie类并写注释（别人的代码真优雅）

--- utils/trie.py
+++ utils/trie.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import collections
 class TrieNode:
     # Initialize your data structure here.
     def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
+        self.children = collections.defaultdict(TrieNode)  # 通过这个defaultdict,使得可以只用一个for循环遍历构造trie
         self.is_word = False
 
 class Trie:
@@ -12,20 +13,30 @@ class Trie:
     def insert(self, word):
         
         current = self.root
-        for letter in word:
+        for letter in word:  # 因为用了defaultdict，不需要递归，直接这样循环就可以建起来树了
             current = current.children[letter]
-        current.is_word = True
+        current.is_word = True  # 修改节点标记，表示这个点到根的路径是一个词
 
     def search(self, word):
+        """
+        在字典树里查找输入词，不仅要适配，还要有词结束的标记
+        :param word:
+        :return:
+        """
         current = self.root
         for letter in word:
             current = current.children.get(letter)
 
-            if current is None:
+            if current is None:  # 因为default dict的关系，如果没有这条分支，取到的值就是none
                 return False
         return current.is_word
 
     def startsWith(self, prefix):
+        """
+        在字典树里查找输入词(前缀)，只需要适配就可以了
+        :param prefix:
+        :return:
+        """
         current = self.root
         for letter in prefix:
             current = current.children.get(letter)
@@ -33,8 +44,14 @@ class Trie:
                 return False
         return True
 
-
     def enumerateMatch(self, word, space="_", backward=False):
+        """
+        枚举查找输入序列在字符串里能找到的所有组合，每次枚举后舍去输入的最后一个字符
+        即查找输入字符串的所有满足的前缀？？
+        单字符的时候不处理（因为那是character based网络处理的内容）
+        :param prefix:
+        :return:
+        """
         matched = []
         ## while len(word) > 1 does not keep character itself, while word keed character itself
         while len(word) > 1:
