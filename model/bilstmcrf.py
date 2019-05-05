0754154a7fbac51c11e97ac8e0ec55417263a324
LiXianyao <18976753003@163.com>
2019-05-05 23:42:50 +0800

    训练部分的代码阅读及注释追加，未完

--- model/bilstmcrf.py
+++ model/bilstmcrf.py
@@ -17,14 +17,17 @@ class BiLSTM_CRF(nn.Module):
         super(BiLSTM_CRF, self).__init__()
         print("build batched lstmcrf...")
         self.gpu = data.HP_gpu
-        ## add two more label for downlayer lstm, use original label size for CRF
-        label_size = data.label_alphabet_size
-        data.label_alphabet_size += 2
+        ## add two more label for downlayer lstm, use original label size for CRF？？？？
+        label_size = data.label_alphabet_size  # 原值存临时
+        data.label_alphabet_size += 2 ## ！！data中的被修改了
         self.lstm = BiLSTM(data)
         self.crf = CRF(label_size, self.gpu)
 
 
     def neg_log_likelihood_loss(self, gaz_list, word_inputs, biword_inputs, word_seq_lengths,  char_inputs, char_seq_lengths, char_seq_recover, batch_label, mask):
+        """ 调用模型，计算得到损失和标记序列
+            内容与 forward基本一致，只不过多了一个调用crf层的损失计算及返回
+        """
         outs = self.lstm.get_output_score(gaz_list, word_inputs, biword_inputs, word_seq_lengths,  char_inputs, char_seq_lengths, char_seq_recover)
         batch_size = word_inputs.size(0)
         seq_len = word_inputs.size(1)
@@ -34,6 +37,7 @@ class BiLSTM_CRF(nn.Module):
 
 
     def forward(self, gaz_list, word_inputs, biword_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover, mask):
+        """ 只在eval时被使用，与neg_log_likelihood_loss的处理只差了一个损失计算 """
         outs = self.lstm.get_output_score(gaz_list, word_inputs, biword_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover)
         batch_size = word_inputs.size(0)
         seq_len = word_inputs.size(1)
